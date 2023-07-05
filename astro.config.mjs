fb4e05227e09d700603fb4dd3310e9ccdc0753f0
sman247 <115250473+sman247@users.noreply.github.com>
2023-07-05 11:17:24 +0100

    commit name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "sman247.github.io",
+    site: "https://sman247.github.io",
     base: "/website",
 });
