1c4068c55b9afdcb27529a1dea92bdcf9115bb6a
sman247 <115250473+sman247@users.noreply.github.com>
2023-07-05 12:35:47 +0100

    tinkering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://sman247.github.io",
-    base: "/website",
+    // site: "https://sman247.github.io",
+    // base: "/website",
+    site: "https://seanmansfield.com",
+    base: "/",
 });
